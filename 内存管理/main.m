c8a8599df6ef2f7323dba2f5fc732e1dbebfc48b
Mersace <ma815305712@outlook.com>
2016-11-24 20:24:21 +0800

    Initial Commit

---
+++ 内存管理/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  内存管理
+//
+//  Created by Ashscar on 16/11/24.
+//  Copyright © 2016年 Ashscar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
